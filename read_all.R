80875c66e1d122539018acd3062b394359975227
Deisy Gysi <deisygysi@gmail.com>
2019-03-27 11:25:00 +0100

    Update read_all.R

--- read_all.R
+++ read_all.R
@@ -6,5 +6,12 @@ for ( i in 1:length(files)){
   name = gsub(pattern = '_Network.csv', 
               replacement = '',
               x = files[i])
-  assign(name, read.csv(files[i]))
+  ### Filter the data in the loop
+  x = read.csv(files[i])
+  x = x[,c(1:3,5)]
+  x$wTO_sign = ifelse(x$pval_sig< 0.05, x$wTO_sign, 0)
+  assign(name, x)
 }
+
+
+require(wTO)
